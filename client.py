37f5e9e8c8eca142d4e735b3cd3e397120b3033b
Lara Aydin <ayd.dilara@gmail.com>
2017-10-20 19:52:03 -0400

    Fix division error and output IP address instead of hostname

--- client.py
+++ client.py
@@ -3,9 +3,8 @@ import socket
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a socket object
 
-host = "0.0.0.0" # socket.gethostname() # gets local machine name
-
-port = 7264
+host = socket.gethostbyname(socket.gethostname()) # '0.0.0.0' # socket.gethostname() # gets local machine name or s
+port = 7261
 
 s.connect((host, port)) # connection to hostname on the port
 print("Connected with server on %s" % str(host))
@@ -14,8 +13,8 @@ while True:
     # receive no more than 1024 bytes
     user_input = raw_input().replace(" ", "")
     if user_input == '0/0':
-        print("User input ends; end the client program")
+        print("User input ends; end the client program.")
         s.close()
         break
-    s.send(user_input)
+    s.send(str(user_input))
     print(s.recv(1024))
--- server.py
+++ server.py
@@ -3,31 +3,42 @@ import socket
 import re
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # creates a socket object
-host = "0.0.0.0" # socket.gethostname() # gets local machine name
-port = 7264
+host =  socket.gethostbyname(socket.gethostname()) # '0.0.0.0' # socket.gethostname() # gets local machine name or socket.gethostbyname(socket.gethostname())
+port = 7261
 
 serversocket.bind((host, port)) # binds to the port
 serversocket.listen(5) # queue up to 5 requests
 
 def calculate(question):
-    regexparse = re.compile(r'^(\.\d+|\d+(\.\d+)?)\s*([+/*-])\s*(\.\d+|\d+(\.\d+)?)$')
+    regexparse = re.compile(r'^(\.\d+|\d+(?:\.\d+)?)\s*([+/*-])\s*(\.\d+|\d+(?:\.\d+)?)$')
 
     if regexparse.match(question):
-        return "Answer from server: {}".format(eval(question))
+        elem = re.findall(regexparse, question)
+        first = float(elem[0][0])
+        op = elem[0][1]
+        second = float(elem[0][2])
+
+        if op == '/' and first % second != 0:
+            return str(first / second)
+        else:
+            return str(eval(question))
     else:
-        return "Invalid question! Please enter the math question again."
+        raise ValueError
 
 while True:
     clientsocket, addr = serversocket.accept() # establishes a connection
-    print("Connected by client on %s" % str(addr))
+    print("Connected by client on %s" % str(host))
 
     while True:
         question = clientsocket.recv(1024)
         if not question:
-            print("Server program ends due to the client closing the connection")
+            print("Server program ends due to the client closing the connection.")
             serversocket.close()
             clientsocket.close()
             exit()
-        answer = calculate(question)
-        print("Received question {}; send back answer {}".format(question, answer))
-        clientsocket.send(answer)
+        try:
+            answer = calculate(question)
+            clientsocket.send("Answer from server: {}".format(answer))
+            print("Received question \"{}\"; send back answer {}".format(question, answer))
+        except ValueError:
+            clientsocket.send("Invalid question! Please enter the math question again.")
